4930f57079b412ce7732f463e7d238c397b8749b
prakash-pun7 <punbishal777@gmail.com>
2021-06-05 21:02:10 +0545

    inital commit

---
+++ script.js
@@ -0,0 +1,240 @@
+var map = L.map("map", {
+   zoomControl: true,
+}).setView([28.213248, 83.9876608], 15); // leaflet plugin with the variable = map
+
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+L.tileLayer(tileUrl, {
+   attribution: attribution,
+   detectRetina: true
+}).addTo(map);
+
+lc = L.control
+   .locate({
+      strings: {
+         title: "Show me where I am, yo!",
+      },
+}).addTo(map);
+
+var searchControl = L.esri.Geocoding.geosearch().addTo(map);
+
+var results = L.layerGroup().addTo(map);
+searchControl.on("results", function (data) {
+   results.clearLayers();
+   for (var i = data.results.length - 1; i >= 0; i--) {
+      results.addLayer(L.marker(data.results[i].latlng));
+      control.spliceWaypoints(0, 1, data.latlng);
+      console.log(data.results[i].latlng);
+   }
+});
+
+// var marker = L.marker([28.213248, 83.9876608]).addTo(map); 
+
+function createButton(label, container) {
+   var btn = L.DomUtil.create("button", "", container);
+   btn.setAttribute("type", "button");
+   btn.innerHTML = label;
+   return btn;
+}
+
+map.on("click", function (e) {
+   var container = L.DomUtil.create("div"),
+      startBtn = createButton("Start from this location", container),
+      endBtn = createButton("Go to this location", container);
+
+   L.popup().setContent(container).setLatLng(e.latlng).openOn(map);
+   L.DomEvent.on(startBtn, "click", function () {
+      control.spliceWaypoints(0, 1, e.latlng);
+      map.closePopup();
+   });
+
+   L.DomEvent.on(endBtn, "click", function () {
+      control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+      map.closePopup();
+   });
+});
+
+var greenIcon = new L.Icon({
+   iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+   shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png",
+   iconSize: [25, 41],
+   iconAnchor: [12, 41],
+   popupAnchor: [1, -34],
+   shadowSize: [41, 41],
+});
+
+var redIcon = new L.Icon({
+   iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+   shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png",
+   iconSize: [25, 41],
+   iconAnchor: [12, 41],
+   popupAnchor: [1, -34],
+   shadowSize: [41, 41],
+});
+
+var blueIcon = new L.Icon({
+   iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
+   shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png",
+   iconSize: [25, 41],
+   iconAnchor: [12, 41],
+   popupAnchor: [1, -34],
+   shadowSize: [41, 41],
+});
+
+// map.on("click", function (e) {
+//    console.log(e);
+//    var newMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+var control = L.Routing.control({
+   // adding waypoints to the route in the control = variable
+   waypoints: [
+      // L.latLng(splitfirst, splitsecond),
+      // L.latLng(last1, last2),
+
+   ],
+   createMarker: function (waypointIndex, waypoint, numberOfWaypoints) {
+      if (waypointIndex === 0) {
+         return L.marker(waypoint.latLng, {
+            draggable: true,
+            icon: greenIcon
+         }).bindPopup("Start");
+      } else if (waypointIndex === numberOfWaypoints - 1) {
+         return L.marker(waypoint.latLng, {
+            icon: redIcon,
+            draggable: true,
+         }).bindPopup("End");
+      } else {
+         return L.marker(waypoint.latLng, {
+            icon: blueIcon
+         });
+      }
+   },
+
+   routeWhileDragging: true,
+   reverseWaypoints: true,
+   showAlternatives: true,
+   autoRoute: true,
+   altLineOptions: {
+      styles: [{
+            color: "black",
+            opacity: 0.15,
+            weight: 10,
+         },
+         {
+            color: "white",
+            opacity: 0.8,
+            weight: 7,
+         },
+         {
+            color: "blue",
+            opacity: 0.5,
+            weight: 3,
+         },
+      ],
+   },
+
+   geocoder: L.Control.Geocoder.nominatim(),
+   // unknown address with latitude and longitude -> "N38.1086, W122.1762"
+   waypointNameFallback: function (latLng) {
+      function zeroPad(n) {
+         n = Math.round(n);
+         return n < 10 ? "0" + n : n;
+      }
+
+      function sexagesimal(p, pos, neg) {
+         var n = Math.abs(p),
+            degs = Math.floor(n),
+            mins = (n - degs) * 60,
+            secs = (mins - Math.floor(mins)) * 60,
+            frac = Math.round((secs - Math.floor(secs)) * 100);
+         return ((n >= 0 ? pos : neg) + degs + "°" + zeroPad(mins) + "'" + zeroPad(secs) + "." + zeroPad(frac) + '"');
+      }
+
+      return (sexagesimal(latLng.lat, "N", "S") + " " + sexagesimal(latLng.lng, "E", "W"));
+   },
+}).addTo(map);
+
+control.on("routesfound", function (e) {
+   var routes = e.routes;
+   console.log(routes);
+   console.log("Found " + routes.length + " route(s).");
+
+   distance = e.routes[0].summary.totalDistance;
+   console.log("routing distance: " + distance);
+
+   var routeDistance = (Math.round(distance / 100) / 10) + " km";
+   console.log(routeDistance);
+
+   time = e.routes[0].summary.totalTime;
+   console.log("routing time: " + time);
+
+   start = e.routes[0].waypoints[0].name;
+   console.log("Start Point:", start);
+
+   end = e.routes[0].waypoints[1].name;
+   console.log("End Point:", end);
+
+   startpoint = e.routes[0].inputWaypoints[0].latLng;
+   console.log("Start Point Latlang:", startpoint);
+   var starting = [startpoint.lat, startpoint.lng];
+   console.log(starting);
+
+   endpoint = e.routes[0].inputWaypoints[1].latLng;
+   console.log("End Point Latlang:", endpoint);
+   var destination = [endpoint.lat, endpoint.lng];
+   console.log(destination);
+
+   var distance = document.getElementById("distance");
+   var total = routeDistance;
+   distance.value = total;
+
+   var startLoc = document.getElementById("start");
+   var loc = start;
+   startLoc.value = loc;
+
+   var startPoint = document.getElementById("startLatlng");
+   var startLatLng = starting;
+   startPoint.value = startLatLng;
+
+   var endPoint = document.getElementById("endLatlng");
+   var endLatLng = destination;
+   endPoint.value = endLatLng;
+
+}).addTo(map);
+
+control.on("routesfound", function (e) {
+   var waypoints = e.waypoints || [];
+   var destination = waypoints[waypoints.length - 1]; // there you have the destination point between your hands
+   console.log("Destination Points");
+   console.log(destination);
+   var end = destination.name;
+   console.log(end);
+
+   var endLoc = document.getElementById("end");
+   var loc = end;
+   endLoc.value = loc;
+   // document.getElementById("end").textContent = end;
+});
+
+
+L.Routing.errorControl(control).addTo(map);
+
+
+if ("geolocation" in navigator) {
+   navigator.geolocation.getCurrentPosition(function (position) {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      console.log(lat, lon);
+      var marker = L.marker([lat, lon]).addTo(map);
+      L.circle([lat, lon, {
+         radius: 1000,
+         opacity: 1,
+         color: "white",
+         weight: 1,
+         fillOpacity: 1,
+         fillColor: "blue",
+      }, ]).addTo(map);
+   });
+} else {
+   console.log("geolocation not available");
+}
